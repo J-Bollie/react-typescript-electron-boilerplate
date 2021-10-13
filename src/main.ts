2c63488f5c67d495d549b85f323b810451a35715
J-Bollie <bollenjensen@gmail.com>
2021-10-13 23:04:38 +0200

    Added dev variable

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,23 @@ const createWindow = (): void => {
   let win = new BrowserWindow({
     width: 800,
     height: 600,
+    show: false,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
       preload: path.resolve(__dirname, "preload.bundle.js")
     }
   });
-  //win.loadURL(`http://localhost:9000/`);
-  win.loadURL(`file://${__dirname}/index.html`);
+
+  win.loadURL(isDev() ? `http://localhost:9000/` : `file://${__dirname}/index.html`).then(() => {
+    win.show();
+  }).catch((err: string) => {
+    console.log(err);
+  });
+
+  function isDev() {
+    return process.argv[2] == '--dev';
+  }
 }
 
 ipcMain.on("test", (e, args) => {
