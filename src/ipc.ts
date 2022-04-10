59b5e940566d6fa68b2c11f348339a4340d8b5f0
Jensen Bollen <48119536+J-Bollie@users.noreply.github.com>
2022-04-10 19:59:37 +0200

    Update ipc.ts

--- src/ipc.ts
+++ src/ipc.ts
@@ -1,5 +1,30 @@
 import { app, BrowserWindow, ipcMain } from "electron";
 
+ipcMain.on("toMain", async (e, args) => {
+  const command = args[0].command;
+  const sender = BrowserWindow.fromWebContents(e.sender);
+
+  switch (command) {
+    case "minimize":
+      sender?.isMinimized() ? null : sender?.minimize();
+      break;
+    case "maximize":
+      sender?.isMaximized() ? sender?.unmaximize() : sender?.maximize();
+      sender?.isMaximized()
+        ? sender?.setMovable(false)
+        : sender?.setMovable(true);
+      break;
+    case "close":
+      app.quit();
+      break;
+    case "loadTheme":
+      break;
+    case "changeTheme":
+      await changeTheme(command);
+      break;
+  }
+});
+
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////
 ////////////////Theme functions////////////////////////
