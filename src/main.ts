1020a9cd6abee0ea5e4ed6906a47e16e8911e7aa
Jensen Bollen <48119536+J-Bollie@users.noreply.github.com>
2022-04-10 19:59:25 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -17,29 +17,4 @@ const createWindow = () => {
   loadTheme(global.mainWindow);
 };
 
-ipcMain.on("toMain", async (e, args) => {
-  const command = args[0].command;
-  const sender = BrowserWindow.fromWebContents(e.sender);
-
-  switch (command) {
-    case "minimize":
-      sender?.isMinimized() ? null : sender?.minimize();
-      break;
-    case "maximize":
-      sender?.isMaximized() ? sender?.unmaximize() : sender?.maximize();
-      sender?.isMaximized()
-        ? sender?.setMovable(false)
-        : sender?.setMovable(true);
-      break;
-    case "close":
-      app.quit();
-      break;
-    case "loadTheme":
-      break;
-    case "changeTheme":
-      await changeTheme(command);
-      break;
-  }
-});
-
 app.on("ready", createWindow);
