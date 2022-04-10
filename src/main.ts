e1aafef5bc76c7aece105f649d1e394d32e256a6
J-Bollie <bollenjensen@gmail.com>
2022-04-10 20:04:54 +0200

    Changes

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 import { app, BrowserWindow, ipcMain } from "electron";
 import { createMainWindow } from "@/browserWindows";
 
-import Store from "electron-store";
-const store = new Store();
+import "./ipc.ts";
+import { loadTheme } from "./ipc";
 
 declare global {
-  namespace NodeJS {
-    interface Global {
-      mainWindow: BrowserWindow | null;
-    }
-  }
+	namespace NodeJS {
+		interface Global {
+			mainWindow: BrowserWindow | null;
+		}
+	}
 }
 
 const createWindow = () => {
-  global.mainWindow = createMainWindow();
-  loadTheme(global.mainWindow);
+	global.mainWindow = createMainWindow();
+	loadTheme(global.mainWindow);
 };
 
 app.on("ready", createWindow);
--- webpack.react.js
+++ webpack.react.js
@@ -1,71 +1,70 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 let config = {
-  entry: './src/renderer.tsx',
-  target: 'electron-renderer',
-  devtool: 'source-map',
-  resolve: {
-    alias: {
-      ['@']: path.resolve(__dirname, 'src')
-    },
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  module: {
-    rules: [{
-        test: /\.ts(x?)$/,
-        include: /src/,
-        use: [{
-          loader: 'ts-loader'
-        }]
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|jp2|webp)$/,
-        loader: 'file-loader',
-        options: {
-          name: 'images/[name].[ext]'
-        }
-      },
-      {
-        test: /\.svg$/,
-        use: ['@svgr/webpack', 'url-loader'],
-      },
-    ]
-  },
-  output: {
-    path: __dirname + '/dist',
-    filename: 'renderer.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: './src/index.html'
-    })
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    hot: true,
-    // open: true,
-    compress: true,
-    port: 9000,
-  },
-}
+	entry: "./src/renderer.tsx",
+	target: "electron-renderer",
+	devtool: "source-map",
+	resolve: {
+		alias: {
+			["@"]: path.resolve(__dirname, "src"),
+		},
+		extensions: [".tsx", ".ts", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts(x?)$/,
+				include: /src/,
+				use: [
+					{
+						loader: "ts-loader",
+					},
+				],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(png|jpe?g|gif|jp2|webp)$/,
+				loader: "file-loader",
+				options: {
+					name: "images/[name].[ext]",
+				},
+			},
+			{
+				test: /\.svg$/,
+				use: ["@svgr/webpack", "url-loader"],
+			},
+		],
+	},
+	output: {
+		path: __dirname + "/dist",
+		filename: "renderer.js",
+		hashFunction: "xxhash64",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: "./src/index.html",
+		}),
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "dist"),
+		},
+		hot: true,
+		// open: true,
+		compress: true,
+		port: 9000,
+	},
+};
 
 module.exports = (env, argv) => {
+	if (argv.mode === "development") {
+		config.target = "web";
+	}
 
-  if (argv.mode === "development") {
-    config.target = "web";
-  }
-
-  return config;
+	return config;
 };
